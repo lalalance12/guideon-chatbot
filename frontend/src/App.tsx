9e6bf405ad2a3c0ae633ae73409688687dc7ff07
Kent Nasvil Madrid <itsfrnasviil@gmail.com>
2025-03-09 15:51:35 +0800

    added dashboard with messages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,17 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar, { SidebarItem } from "./components/Sidebar";
 import { MessageSquarePlus, Settings } from "lucide-react";
 import Dashboard from "./components/Dashboard";
+import "typeface-muli";
 
 const App = (): React.ReactElement => {
   return (
-    <main className="flex gap-4 h-screen">
+    <main className="flex gap-4 h-screen font-muli bg-surface-a0">
       <Sidebar>
         <SidebarItem
           icon={<MessageSquarePlus size={20} />}
           text="New Message"
         />
-        <SidebarItem icon={<Settings size={20} />} text="Settings" />
+        <SidebarItem icon={<Settings size={20} />} text="Settings" active />
       </Sidebar>
       <Dashboard />
     </main>
