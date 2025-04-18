c6ce68a430a2dbca806a71ad4df2c86d4d5626d7
Kent Nasvil Madrid <itsfrnasviil@gmail.com>
2025-04-18 19:22:23 +0800

    add default renderer and parser classes to Django REST framework settings for improved API response handling

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,15 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ],
 }
 
 SIMPLE_JWT = {
