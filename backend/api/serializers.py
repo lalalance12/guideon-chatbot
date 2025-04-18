3717ebf8a88211506b38e1cdb89b5480da18a062
Kent Nasvil Madrid <itsfrnasviil@gmail.com>
2025-04-18 19:22:04 +0800

    enhance UserSerializer to include password validation and streamline user creation process

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,28 +1,30 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
     fullName = serializers.CharField(source='first_name', required=True)
     email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'fullName']
         extra_kwargs = {
-            'password': {'write_only': True, 'required': True},
-            'username': {'required': False}
+            'username': {'required': False}  # We'll set this in create()
         }
 
     def create(self, validated_data):
         # Extract first_name from validated_data
         first_name = validated_data.pop('first_name', '')
         email = validated_data.pop('email')
+        password = validated_data.pop('password')
         
         # Create user with email as username
         user = User.objects.create_user(
-            username=email,
+            username=email,  # Use email as username
             email=email,
             first_name=first_name,
-            **validated_data
+            password=password
         )
         return user
