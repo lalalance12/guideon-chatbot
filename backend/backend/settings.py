77fe63c3aa3ef3d7261cedbbeaf4875b63d14b0e
Noobery <alrickgicole@gmail.com>
2025-05-17 11:51:34 +0800

    Update SIMPLE_JWT token lifetimes for improved security and usability

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,8 +50,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=6),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
 
